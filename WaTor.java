ed1054d9f78a0e1cb6448b402d908cb5b8f77ef7
kkemp3 <kkemp3@wisc.edu>
2018-04-26 19:54:36 -0500

    savePopulationMethod
    I think this method is done

--- WaTor.java
+++ WaTor.java
@@ -1289,9 +1289,42 @@ class WaTor {
 	public static void savePopulationChart(int[] simulationParameters, ArrayList<int[]> history, int oceanWidth,
 			int oceanHeight, String filename) throws IOException {
 		// test comment
+		if (simulationParameters == null || history == null) {
+			System.out.println("Error message, null value");
+			return;
+		}
+		try {
 		File file = new File(filename);
+		PrintWriter printWriter = new PrintWriter(file);
 		for (int i = 0; i < simulationParameters.length; i++) {
-			
+			printWriter.println(simulationParameters[i]);
+		}
+		printWriter.println();
+		for (int i = 0; i < history.length; i++) {
+			int chronon = history[i][0]; // get chronon
+			int fish = history[i][1]; // get number of fish
+			int sharks = history[i][2]; // get number of sharks
+			printWriter.printf("F%3d", fish); // print "F #"
+			printWriter.print(" "); // print space
+			printWrtier.printf("F%3d", sharks); // print "S #"
+			printWriter.print(" "); // print space
+			printWriter.print(chronon + (")"); // print "chronon)"
+			if (fish < sharks) {  // print fish first
+				for (int i =0; i < fish; i++) 
+					printWRtier.print("."); // print .'s for fish
+			for (int i = 0; i < sharks; i++) 
+				printWriter.print("O"); // print O's for sharks
+
+			printWriter.println();
+			} else { // print sharks first
+				for (int i = 0; i < sharks; i++) 
+					printWriter.print("O"); // print O's for sharks
+				for (int i =0; i < fish; i++) 
+					printWRtier.print("."); // print .'s for fish
+			}
+		}
+		} catch (IOException e) {
+			System.out.println("File cannot be written.")
 		}
 		
 		
